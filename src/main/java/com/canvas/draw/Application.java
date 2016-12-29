f23722b548eaf4b7ee4455d29a474ab8ddc3e893
Lazar Agatonovic <lazakg@gmail.com>
2016-12-29 13:57:05 +0100

    Added help command

--- src/main/java/com/canvas/draw/Application.java
+++ src/main/java/com/canvas/draw/Application.java
@@ -7,32 +7,30 @@ import java.util.Scanner;
  */
 public class Application {
 
+    private static final String HELP_COMMAND = "help";
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
         final Worksheet worksheet = new Worksheet();
 
         while (true) {
             final String command = scanner.nextLine();
-            if (!worksheet.execute(command)) {
+            if (command.equals(HELP_COMMAND)) {
+                displayHelp();
+            } else if (!worksheet.execute(command)) {
                 break;
             }
         }
         System.out.print("Bye...");
-//        Canvas canvas = new Canvas(20, 4);
-//        Line line1 = new Line(new Point(1, 2), new Point(6, 2));
-//        Line line2 = new Line(new Point(6, 3), new Point(6, 4));
-//        Line line3 = new Line(new Point(4, 3), new Point(16, 3));
-//        Rectangle rec1 = new Rectangle(new Point(16, 1), new Point(20, 3));
-//        canvas.addDrawable(line1);
-//        canvas.addDrawable(line2);
-//        canvas.addDrawable(line3);
-//        canvas.addDrawable(rec1);
-//        canvas.paint(new Point(10, 2), 'o');
-//        canvas.paint(new Point(6, 2), '-');
-//        canvas.paint(new Point(1, 4), '8');
-//        canvas.paint(new Point(15, 4), '*');
-//        canvas.draw();
     }
 
+    private static void displayHelp() {
+        System.out.println("C w h          Creates a new canvas of width w and height h.");
+        System.out.println("L x1 y1 x2 y2  Creates a line from (x1, y1) to (x2, y2) using `x` character.");
+        System.out.println("R x1 y1 x2 y2  Creates a rectangle whose diagonal vertexes are (x1, y1) to (x2, y2)" +
+                " using `x` character for the outline.");
+        System.out.println("B x y c        Fills in entire area connected to (x, y) using colour 'c'.");
+        System.out.println("Q              Exits the program.");
+
+    }
 
 }
